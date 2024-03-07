f343719feba9aeaf537b126a58caceb0deab5aa8
rudenkoserhii <rudenko.sergey.v@gmail.com>
2024-03-07 17:26:43 +0200

    feat: made footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,36 +1,152 @@
-import { Footer } from 'flowbite-react'
+import { Footer, Modal } from 'flowbite-react'
 import {
   BsDribbble,
   BsFacebook,
   BsGithub,
   BsInstagram,
-  BsTwitter,
+  BsTwitterX,
+  BsLinkedin,
 } from 'react-icons/bs'
-import React from 'react'
+import { SiFreelancer, SiUpwork } from 'react-icons/si'
+
+import React, { useState } from 'react'
+import { URL } from '../../constants'
+import { ReactComponent as icon } from '../../asserts/icons/logo.svg'
+import { ReactComponent as title } from '../../asserts/icons/title.svg'
+import { ReactComponent as bsky } from '../../asserts/icons/bsky.svg'
 
 function FooterComponent(): React.JSX.Element {
+  const [openModal, setOpenModal] = useState<boolean>(false)
   return (
-    <Footer container>
+    <Footer container className="bg-[#dafc00]">
       <div
-        className="w-full justify-between sm:flex
-            sm:items-center sm:justify-between"
+        className="justify-between flex-col gap-6
+                  w-full flex items-center sm:flex-row"
       >
-        <Footer.Brand
-          href="https://flowbite.com"
-          src="https://flowbite.com/docs/images/logo.svg"
-          alt="Flowbite Logo"
-          name="Flowbite"
-        />
-        <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-          <Footer.LinkGroup>
-            <Footer.Icon href="#" icon={BsFacebook} />
-            <Footer.Icon href="#" icon={BsInstagram} />
-            <Footer.Icon href="#" icon={BsTwitter} />
-            <Footer.Icon href="#" icon={BsGithub} />
-            <Footer.Icon href="#" icon={BsDribbble} />
+        <div className="flex gap-4 items-center">
+          <Footer.Icon
+            title="ToDo Manager"
+            href={URL.homePage}
+            icon={icon}
+            className="[&>svg]:fill-[#8442b9] [&>svg]:transition-all
+[&>svg]:w-auto [&>svg]:h-[2rem] [&>svg]:hover:fill-[#a556e6]
+[&>svg]:hover:transition-all"
+          />
+          <Footer.Icon
+            title="ToDo Manager"
+            href={URL.homePage}
+            icon={title}
+            className="[&>svg]:fill-[#8442b9] [&>svg]:transition-all
+[&>svg]:w-auto [&>svg]:h-[1rem] [&>svg]:hover:fill-[#a556e6]
+[&>svg]:hover:transition-all"
+          />
+        </div>
+        <Footer.LinkGroup>
+          <Footer.Title
+            title="Contacts"
+            className="mb-0 text-[#8442b9] cursor-pointer transition-all
+                      hover:text-[#a556e6] hover:transition-all"
+            onClick={(): void => setOpenModal(true)}
+          />
+        </Footer.LinkGroup>
+        <div className="flex space-x-6 sm:justify-center">
+          <Footer.LinkGroup className="gap-2">
+            <Footer.Icon
+              title="Facebook"
+              href={URL.facebook}
+              icon={BsFacebook}
+              className="[&>svg]:fill-[#8442b9] [&>svg]:transition-all
+                      [&>svg]:hover:fill-[#a556e6] [&>svg]:hover:transition-all"
+            />
+            <Footer.Icon
+              title="Instagram"
+              href={URL.instagram}
+              icon={BsInstagram}
+              className="[&>svg]:fill-[#8442b9] [&>svg]:transition-all
+                      [&>svg]:hover:fill-[#a556e6] [&>svg]:hover:transition-all"
+            />
+            <Footer.Icon
+              title="TwitterX"
+              href={URL.twitter}
+              icon={BsTwitterX}
+              className="[&>svg]:fill-[#8442b9] [&>svg]:transition-all
+                      [&>svg]:hover:fill-[#a556e6] [&>svg]:hover:transition-all"
+            />
+            <Footer.Icon
+              title="GitHub"
+              href={URL.github}
+              icon={BsGithub}
+              className="[&>svg]:fill-[#8442b9] [&>svg]:transition-all
+                      [&>svg]:hover:fill-[#a556e6] [&>svg]:hover:transition-all"
+            />
+            <Footer.Icon
+              title="Dribble"
+              href={URL.dribble}
+              icon={BsDribbble}
+              className="[&>svg]:fill-[#8442b9] [&>svg]:transition-all
+                      [&>svg]:hover:fill-[#a556e6] [&>svg]:hover:transition-all"
+            />
+            <Footer.Icon
+              title="LinkedIn"
+              href={URL.linledin}
+              icon={BsLinkedin}
+              className="[&>svg]:fill-[#8442b9] [&>svg]:transition-all
+                      [&>svg]:hover:fill-[#a556e6] [&>svg]:hover:transition-all"
+            />
+            <Footer.Icon
+              title="Freelancer"
+              href={URL.freelancer}
+              icon={SiFreelancer}
+              className="[&>svg]:fill-[#8442b9] [&>svg]:transition-all
+                      [&>svg]:hover:fill-[#a556e6] [&>svg]:hover:transition-all"
+            />
+            <Footer.Icon
+              title="Upwork"
+              href={URL.upwork}
+              icon={SiUpwork}
+              className="[&>svg]:fill-[#8442b9] [&>svg]:transition-all
+                      [&>svg]:hover:fill-[#a556e6] [&>svg]:hover:transition-all"
+            />
+            <Footer.Icon
+              title="BlueSky"
+              href={URL.bsky}
+              icon={bsky}
+              className="[&>svg]:fill-[#8442b9] [&>svg]:transition-all
+                      [&>svg]:hover:fill-[#a556e6] [&>svg]:hover:transition-all"
+            />
           </Footer.LinkGroup>
         </div>
       </div>
+      <Modal
+        dismissible
+        show={openModal}
+        onClose={(): void => setOpenModal(false)}
+      >
+        <Modal.Header
+          className="[&>h3]:text-[#8442b9] [&>button]:text-[#a556e6]
+        [&>button]:transition-all
+                              hover:[&>button]:text-[#8442b9]
+        hover:[&>button]:bg-[#caa4ea] hover:[&>button]:transition-all"
+        >
+          Contacts
+        </Modal.Header>
+        <Modal.Body>
+          <Footer.LinkGroup className="justify-center">
+            <Footer.Link
+              className="text-[#a556e6]"
+              href="mailto:rudenko.serhii.v@gmail.com"
+            >
+              rudenko.serhii.v@gmail.com
+            </Footer.Link>
+            <Footer.Link className="text-[#a556e6]" href="tel:+380504521144">
+              +380 50 452 11 44
+            </Footer.Link>
+          </Footer.LinkGroup>
+        </Modal.Body>
+        <Modal.Footer className="justify-center">
+          <p className="text-[#8442b9] font-sm text-center">Happy Coding!</p>
+        </Modal.Footer>
+      </Modal>
     </Footer>
   )
 }
---
+++ src/constants/url.ts
@@ -0,0 +1,12 @@
+export const URL = {
+  'homePage': 'https://todomanager.vercel.app',
+  'dribble': 'https://dribbble.com/rudenkoserhii',
+  'twitter': 'https://twitter.com/SerhiiRudenkoV/',
+  'facebook': 'https://www.facebook.com/profile.php?id=100005434448335',
+  'instagram': 'https://www.instagram.com/rudenko.sergey.v/',
+  'linledin': 'https://www.linkedin.com/in/serhii-rudenko-559a0215b/',
+  'github': 'https://github.com/rudenkoserhii',
+  'bsky': 'https://bsky.app/profile/rudenkoserhii.bsky.social',
+  'upwork': 'https://www.upwork.com/freelancers/~0174735ddc7cf75ffd?viewMode=1',
+  'freelancer': 'https://www.freelancer.com/u/rudenkoserhii',
+}
